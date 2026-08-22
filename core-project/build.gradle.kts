15bd94da294c551bcc29fad52cf713f581a0deb4
Steven <mail@shansen-online.de>
2026-08-22 21:48:57 +0200

    Add core project module

---
+++ core-project/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+    testImplementation("junit:junit:4.13.2")
+}
