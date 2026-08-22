878ccb92002dab9be866e4ccea3a7203867f748f
Steven <mail@shansen-online.de>
2026-08-22 21:50:23 +0200

    Connect app to RFIDGear project core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":core-project"))
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
